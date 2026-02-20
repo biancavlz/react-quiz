2f40196a57cdb7a648a51c71956d710583205269
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-02-20 14:16:55 +0100

    Add StartScreen component

---
+++ src/StartScreen.js
@@ -0,0 +1,11 @@
+function StartScreen() {
+  return (
+    <div clas>
+      <h2>Welcome to the React quiz!</h2>
+      <h3>X question to test your React skills</h3>
+      <button>Let's start</button>
+    </div>
+  );
+}
+
+export default StartScreen;
