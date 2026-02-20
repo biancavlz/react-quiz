3d5c73a3d9d351201bc33a4cd8cd2f7631b484b2
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-02-20 14:49:48 +0100

    create Question component

--- src/App.js
+++ src/App.js
@@ -9,9 +9,8 @@ import Question from "./Question";
 
 const initialState = {
   questions: [],
-
-  //'loading','error','ready', 'active', 'finished'
-  status: "loading",
+  status: "loading", //'loading','error','ready', 'active', 'finished'
+  index: 0,
 };
 
 function reducer(state, action) {
@@ -28,7 +27,10 @@ function reducer(state, action) {
 }
 
 function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState,
+  );
 
   const numQuestions = questions.length;
 
@@ -48,7 +50,7 @@ function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question />}
+        {status === "active" && <Question question={questions[index]} />}
       </Main>
     </div>
   );
