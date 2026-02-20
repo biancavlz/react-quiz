ff2e7dca2a0c10956c3cbb1c0e5de1be750ae487
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-02-20 14:24:38 +0100

    Add Question component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Main from "./Main";
 import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
+import Question from "./Question";
 
 const initialState = {
   questions: [],
@@ -43,6 +44,7 @@ function App() {
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
         {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+        {status === "active" && <Question />}
       </Main>
     </div>
   );
