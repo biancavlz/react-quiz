1b3dd73bc95234ee0213b8fbe5747bfa5707c2d2
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-02-20 10:21:44 +0100

    Create DateCounter component, use useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import DateCounter from "./DateCounter";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <DateCounter />{" "}
+    </div>
+  );
 }
 
 export default App;
