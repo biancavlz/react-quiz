492fe35a0781ed4a826fc7a17df61bd1e3cf80a2
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-02-21 17:03:49 +0100

    add seconds remaining  functionality

--- src/components/App.js
+++ src/components/App.js
@@ -19,8 +19,10 @@ const initialState = {
   answer: null,
   points: 0,
   hightscore: 0,
-  secondsRemaining: 10,
+  secondsRemaining: null,
 };
+
+const SECS_PER_QUESTION = 30;
 
 function reducer(state, action) {
   switch (action.type) {
@@ -29,7 +31,11 @@ function reducer(state, action) {
     case "dataFailed":
       return { ...state, status: "error" };
     case "start":
-      return { ...state, status: "active" };
+      return {
+        ...state,
+        status: "active",
+        secondsRemaining: state.questions.length * SECS_PER_QUESTION,
+      };
     case "newAnswer":
       const question = state.questions.at(state.index);
       return {
